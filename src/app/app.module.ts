adb611c3f1f8305ffe1e0dd2451d1404a49325c5
lai2-01 <lai2-01@itismeucci.com>
2023-03-14 10:29:36 +0100

    tabella finita, secondo step iniziato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabellinaComponent } from './tabellina/tabellina.component';
+import {MatTableModule} from '@angular/material/table';
+import { BottoneComponent } from './bottone/bottone.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabellinaComponent,
+    BottoneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
